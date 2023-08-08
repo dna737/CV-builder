b7b1a24ba34aeb3b9775dd27139a32d4c46748b8
dna737 <adnanhere27@gmail.com>
2023-08-08 18:10:20 -0500

    rename <Name> to <Field> and complete remaining field for personalIno

--- CV-builder/src/components/ResumeForm.jsx
+++ CV-builder/src/components/ResumeForm.jsx
@@ -1,30 +1,49 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-export default function ResumeForm({ personalInfo, changeInput }) {
+export default function ResumeForm(props) {
     return (
         <div className=" w-1/2 min-w-{50%} h-full bg-red-500 p-2.5 rounded-md">
             <h1>Personal Info && Socials:</h1>
             <form className="bg-yellow-400 p-3">
                 <div className="grid md:grid-cols-2 md:gap-6">
-                    <Name
-                        personalInfo={personalInfo}
-                        changeInput={changeInput}
-                        attribute={"name"}
-                    ></Name>
+                    <Field {...props} attribute={"name"} />
+                    <Field {...props} attribute={"contact"} />
+                    <Field {...props} attribute={"email"} />
+                    <Field {...props} attribute={"github"} />
                 </div>
             </form>
         </div>
     );
 }
 
-function Name({ personalInfo, changeInput, attribute }) {
+function getTypeOfInput(attribute) {
+    let type = null;
+    switch (attribute) {
+        case "contact":
+            type = "tel";
+            break;
+        case "email":
+            type = "email";
+            break;
+        default:
+            type = "text";
+            break;
+    }
+
+    return type;
+}
+
+function Field({ personalInfo, changeInput, attribute }) {
+    let type = getTypeOfInput(attribute);
+
     return (
         <div className="mb-6">
             <div className="first-name-field flex flex-col">
-                <label htmlFor="name">Name</label>
+                <label htmlFor={attribute}>{attribute}</label>
                 <input
-                    type="text"
-                    value={personalInfo.name}
-                    id="name"
+                    type={type}
+                    value={personalInfo[attribute]}
+                    id={attribute}
                     onChange={(e) => {
                         changeInput(attribute, e.target.value);
                     }}
