7394d9babec6ed943cdd2278e9707af1847713dd
dna737 <adnanhere27@gmail.com>
2023-08-08 00:37:56 -0500

    style editing panel and set label for name input

--- CV-builder/src/components/ResumeForm.jsx
+++ CV-builder/src/components/ResumeForm.jsx
@@ -1,27 +1,33 @@
 /* eslint-disable react/prop-types */
 export default function ResumeForm({ personalInfo, changeInput }) {
     return (
-        <form>
-            <Name
-                personalInfo={personalInfo}
-                changeInput={changeInput}
-                attribute={"name"}
-            ></Name>
-        </form>
+        <div className="h-full bg-red-500 p-2.5 rounded-md">
+            <h1>Personal Info && Socials:</h1>
+            <form>
+                <Name
+                    personalInfo={personalInfo}
+                    changeInput={changeInput}
+                    attribute={"name"}
+                ></Name>
+            </form>
+        </div>
     );
 }
 
 function Name({ personalInfo, changeInput, attribute }) {
     return (
         <div className="mb-6">
-            <input
-                type="text"
-                value={personalInfo.name}
-                id="name"
-                onChange={(e) => {
-                    changeInput(attribute, e.target.value);
-                }}
-            ></input>
+            <div className="first-name-field flex flex-col">
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    value={personalInfo.name}
+                    id="name"
+                    onChange={(e) => {
+                        changeInput(attribute, e.target.value);
+                    }}
+                ></input>
+            </div>
         </div>
     );
 }
