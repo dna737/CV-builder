ae7bbe5339e115074cba8e12d8639af547f05240
dna737 <adnanhere27@gmail.com>
2023-08-08 00:58:17 -0500

    fix width of both panels and implement grid within the form

--- CV-builder/src/components/Preview.jsx
+++ CV-builder/src/components/Preview.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/prop-types */
 function Preview({ personalInfo }) {
-    return <h2>{personalInfo.name}</h2>;
+    return (
+        <div className="h-3.5 w-1/2">
+            <h2>{personalInfo.name}</h2>
+        </div>
+    );
 }
 
 export { Preview };
--- CV-builder/src/components/ResumeForm.jsx
+++ CV-builder/src/components/ResumeForm.jsx
@@ -1,14 +1,16 @@
 /* eslint-disable react/prop-types */
 export default function ResumeForm({ personalInfo, changeInput }) {
     return (
-        <div className="h-full bg-red-500 p-2.5 rounded-md">
+        <div className=" w-1/2 min-w-{50%} h-full bg-red-500 p-2.5 rounded-md">
             <h1>Personal Info && Socials:</h1>
-            <form>
-                <Name
-                    personalInfo={personalInfo}
-                    changeInput={changeInput}
-                    attribute={"name"}
-                ></Name>
+            <form className="bg-yellow-400 p-3">
+                <div className="grid md:grid-cols-2 md:gap-6">
+                    <Name
+                        personalInfo={personalInfo}
+                        changeInput={changeInput}
+                        attribute={"name"}
+                    ></Name>
+                </div>
             </form>
         </div>
     );
