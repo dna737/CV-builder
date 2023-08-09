6719ab0db40825edaa6b75727d739f2b5801b46d
dna737 <adnanhere27@gmail.com>
2023-08-08 21:41:35 -0500

    display contact  and social information

--- CV-builder/src/components/Preview.jsx
+++ CV-builder/src/components/Preview.jsx
@@ -1,9 +1,27 @@
 /* eslint-disable react/prop-types */
 function Preview({ personalInfo }) {
+    //TODO: Display the result of the rest of the fields.
     return (
-        <div className="h-3.5 w-1/2">
-            <h2>{personalInfo.name}</h2>
-        </div>
+        <>
+            <div className="p-2 h-full w-1/2 bg-gray-400">
+                <div className="text-center text-3xl font-bold">
+                    <h2>{personalInfo.name}</h2>
+                </div>
+                <div className="flex flex-nowrap ">
+                    <div className="flex-grow underline flex-shrink-0 justify-center flex  ">
+                        {personalInfo.contact}
+                    </div>
+                    <div className="w-1 h-full">|</div>
+                    <div className="flex flex-grow underline flex-shrink-0 justify-center ">
+                        {personalInfo.email}
+                    </div>
+                    <div className="w-1 h-full">|</div>
+                    <div className="flex  flex-grow underline flex-shrink-0 justify-center ">
+                        {personalInfo.github}
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
