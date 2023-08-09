dd33853a5c0f98d0d9d632b5fe4f7bc71b2b14a8
dna737 <adnanhere27@gmail.com>
2023-08-08 19:15:01 -0500

    create functional components for three sections in the form

--- CV-builder/src/components/ResumeForm.jsx
+++ CV-builder/src/components/ResumeForm.jsx
@@ -1,21 +1,51 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-export default function ResumeForm(props) {
+export default function ResumeForm({
+    personalInfo,
+    changePersonalInput,
+    education,
+    changeEducationInput,
+    experience,
+    changeExperienceInput,
+}) {
     return (
         <div className=" w-1/2 min-w-{50%} h-full bg-red-500 p-2.5 rounded-md">
-            <h1>Personal Info && Socials:</h1>
-            <form className="bg-yellow-400 p-3">
-                <div className="grid md:grid-cols-2 md:gap-6">
-                    <Field {...props} attribute={"name"} />
-                    <Field {...props} attribute={"contact"} />
-                    <Field {...props} attribute={"email"} />
-                    <Field {...props} attribute={"github"} />
-                </div>
-            </form>
+            <PersonInfoAndSocials
+                personalInfo={personalInfo}
+                changePersonalInput={changePersonalInput}
+            />
+            <Education
+                educationInfo={education}
+                changeEducationInput={changeEducationInput}
+            />
+            <Experience
+                experienceInfo={experience}
+                changeExperienceInput={changeEducationInput}
+            />
+            ;
         </div>
     );
 }
 
+function PersonInfoAndSocials(props) {
+    return (
+        <>
+            <h1>Personal Info && Socials:</h1>
+            <form className="bg-yellow-400 p-3">
+                <div className="grid md:grid-cols-2 md:gap-6">
+                    <PersonalField {...props} attribute={"name"} />
+                    <PersonalField {...props} attribute={"contact"} />
+                    <PersonalField {...props} attribute={"email"} />
+                    <PersonalField {...props} attribute={"github"} />
+                </div>
+            </form>
+        </>
+    );
+}
+
+function Education(props) {
+    return <></>;
+}
 function getTypeOfInput(attribute) {
     let type = null;
     switch (attribute) {
@@ -33,19 +63,24 @@ function getTypeOfInput(attribute) {
     return type;
 }
 
-function Field({ personalInfo, changeInput, attribute }) {
-    let type = getTypeOfInput(attribute);
+function Experience(props) {
+    return <></>;
+}
 
+function PersonalField({ personalInfo, changePersonalInput, attribute }) {
+    let type = getTypeOfInput(attribute);
     return (
         <div className="mb-6">
             <div className="first-name-field flex flex-col">
-                <label htmlFor={attribute}>{attribute}</label>
+                <label htmlFor={attribute + (attribute === "github" && "ID")}>
+                    {attribute}
+                </label>
                 <input
                     type={type}
                     value={personalInfo[attribute]}
                     id={attribute}
                     onChange={(e) => {
-                        changeInput(attribute, e.target.value);
+                        changePersonalInput(attribute, e.target.value);
                     }}
                 ></input>
             </div>
